d1095025a03f2eca17fd6c70e7083419d2ef0752
Dominic Torruellas <dom@domtorr.com>
2019-06-18 12:53:04 -0700

    5.2 completed

---
+++ 05-Using-Structs/Examples/5-2/e01_rectangles/src/main.rs
@@ -0,0 +1,39 @@
+// fn main() {
+//     let width1 = 30;
+//     let height1 = 50;
+
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(width1, height1)
+//     );
+// }
+
+// fn area(width: u32, height: u32) -> u32 {
+//     width * height
+// }
+
+// Refactored using Structs
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangel is {} square pixels",
+        area(&rect1)
+    );
+
+    println!("rect1 is {:#?}", rect1);
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
